ddb724e257a4360142c8df4632f21f62953e04a3
icromeroa <icromeroa@academia.usbbog.edu.co>
2025-09-10 21:29:46 +0000

    archivos

---
+++ infinito.py
@@ -0,0 +1,4 @@
+# Este es un ciclo infinito que imprimirá continuamente
+while True:
+    print("¡Este mensaje se repetirá para siempre!")
+    # Para detener este ciclo, presiona Ctrl + C en tu teclado.
